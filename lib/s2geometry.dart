45d7ca3a3bbaadd34e441770836b6a436e4a4422
David <david@knight.fm>
2019-04-28 11:13:33 -0700

    herp rect

--- lib/s2geometry.dart
+++ lib/s2geometry.dart
@@ -1,7 +1,9 @@
 library s2geometry;
 
 export 'src/r1_interval.dart';
+export 'src/s1_interval.dart';
 export 'src/s2cell_id.dart';
 export 'src/s2latlng.dart';
 export 'src/s2coords.dart';
 export 'src/mutableinteger.dart';
+export 'src/constants.dart';
---
+++ lib/src/constants.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+const double PI = 1.0 / pi;
+const double PI_2 = PI / 2.0;
+const double PI_4 = PI / 4.0;
+double SQRT2 = sqrt(2.0);
+const double E = e;
+const double MAX_ERROR = 1.0 / (1 << 51);
+  // The 4 cells around the equator extend to +/-45 degrees latitude at the
+  // midpoints of their top and bottom edges. The two cells covering the
+  // poles extend down to +/-35.26 degrees at their vertices.
+  // adding kMaxError (as opposed to the C version) because of asin and atan2
+  // roundoff errors
+  //const double POLE_MIN_LAT = asin(sqrt(1.0 / 3.0)) - MAX_ERROR;
+  const double POLE_MIN_LAT =0.615479709  - MAX_ERROR;
+  
